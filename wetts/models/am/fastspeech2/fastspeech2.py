cfff2e00f27fe552f56850afb76d0a3d649ab509
unrea1-sama <unrea1sama@outlook.com>
2022-05-30 22:29:52 +0800

    remove redundant code from fastspeech2.py and variance_adapter.py

--- wetts/models/am/fastspeech2/fastspeech2.py
+++ wetts/models/am/fastspeech2/fastspeech2.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # Modified from FastSpeech2(https://github.com/ming024/FastSpeech2)
 
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import torch
 from torch import nn
@@ -56,7 +56,7 @@ class FastSpeech2(nn.Module):
         """Initializing FastSpeech2.
 
         Args:
-            enc_hidden_dim (int): _description_
+            enc_hidden_dim (int): d_model for FFT block.
             n_enc_layer (int): Number of layers in encoder.
             n_enc_head (int):  Number of attention heads in encoder.
             n_enc_conv_filter (int): Number of convolution filters in the first
@@ -71,16 +71,18 @@ class FastSpeech2(nn.Module):
             va_conv_kernel_size (int): Kernel size for all convolution layers
             in variance adapter.
             va_dropout (float): Dropout of variance adapter.
-            pitch_min (float): Minimum of pitch in linear domain.
-            pitch_max (float): Maximum of pitch in linear domain.
-            pitch_mean (float): Mean of pitch in linear domain.
-            pitch_sigma (float): Sigma of pitch in linear domain.
-            energy_min (float): Minimum of energy in linear domain.
-            energy_max (float): Maximum of energy in linear domain.
-            energy_mean (float): Mean of energy in linear domain.
-            energy_sigma (float): Sigma of energy in linear domain.
-            n_pitch_bin (int): Number of bins for pitch quantization.
-            n_energy_bin (int): Number of bins for energy quantization.
+            pitch_min (float): Minimum of pitch to construct pitch bins.
+            pitch_max (float): Maximum of pitch to construct pitch bins.
+            pitch_mean (float): Mean of pitch to construct pitch bins.
+            pitch_sigma (float): Standard deviation of pitch to normalize
+            minimum and maximum of pitch.
+            energy_min (float): Minimum of energy to construct energy bins.
+            energy_max (float): Maximum of energy to construct energy bins.
+            energy_mean (float): Mean of energy to construct energy bins.
+            energy_sigma (float): Standard deviation of energy to normalize
+            minimum and maximum of energy.
+            n_pitch_bin (int): Number of pitch bins for pitch quantization.
+            n_energy_bin (int): Number of energy bins for energy quantization.
             n_dec_layer (int): Number of layers in mel decoder.
             n_dec_head (int): Number of attention heads in mel decoder.
             n_dec_conv_filter (int): Number of convolution filters in the
@@ -160,104 +162,69 @@ class FastSpeech2(nn.Module):
         x: torch.Tensor,
         x_length: torch.Tensor,
         x_token_type: torch.IntTensor,
-        duration_target: torch.Tensor,
-        pitch_target: torch.Tensor,
-        energy_target: torch.Tensor,
-        speaker: torch.Tensor = None
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        """Forward function of FastSpeech2 used for training.
-
-        This function should not be used for inference. Teacher-forcing is
-        applied to predict mel.
+        duration_target: Optional[torch.Tensor] = None,
+        pitch_target: Optional[torch.Tensor] = None,
+        energy_target: Optional[torch.Tensor] = None,
+        p_control: float = 1.0,
+        e_control: float = 1.0,
+        d_control: float = 1.0,
+        speaker: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor,
+               torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Calculating mel-spectrogram from input phoneme sequences.
 
         Args:
             x (torch.Tensor): Input phoneme sequences.
-            x_length (torch.Tensor): A Tensor of shape (b), each element is the
-            length of x.
-            x_token_type (torch.IntTensor): An IntTensor of shape (b,t'). Assume
-            the output of the final FFT block is final_FFT_output and the final
-            output of encoder is enc_output. When token_type[i,j] is 0,
-            final_FFT_output[i,j,:] will be remove from enc_output.
-            duration_target (torch.Tensor): shape (b,t). Ground truth duration
-            for each phoneme.
-            pitch_target (torch.Tensor): shape (b,t). Ground truth pitch for
-            each phoneme.
-            energy_target (torch.Tensor): shape (b,t). Ground truth energy for
-            each phoneme.
-            speaker (torch.Tensor, optional): shape (b). Speaker ID for each
-            training sample. Defaults to None.
+            x_length (torch.Tensor): Length of each input phoneme sequence.
+            Shape (b).
+            x_token_type (torch.IntTensor): Token type for each phoneme. 0
+            indicates that the corresponding input phoneme is a special token,
+            which will be removed after encoder. 1 indicates that the
+            corresponding input phoneme is not a special token.
+            duration_target (Optional[torch.Tensor], optional): Ground truth
+            duration for each phoneme. Defaults to None.
+            pitch_target (Optional[torch.Tensor], optional): Ground truth
+            pitch for each phoneme. Defaults to None.
+            energy_target (Optional[torch.Tensor], optional): Ground truth
+            energy for each phoneme. Defaults to None.
+            p_control (float, optional): Pitch manipulation factor. Defaults to
+            1.0.
+            e_control (float, optional): Energy manipulation factor. Defaults to
+            1.0.
+            d_control (float, optional): Duration manipulation factor. Defaults
+            to 1.0.
+            speaker (Optional[torch.Tensor], optional): Speaker ID for each
+            input phoneme sequence. Defaults to None.
 
         Returns:
-            Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-            predicted mel, predicted pitch, predicted energy, predicted duration
-            in log domain.
+            Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor,
+            torch.Tensor, torch.Tensor, torch.Tensor]: Predicted
+            mel-spectrogram, predicted mel-spectrogram from post net,
+            mel-spectrogram mask, predicted pitch, predicted energy, predicted
+            duration,
         """
-        # If x_padding_mask[i,j] is True, x[i,j,:] will be masked in encoder
-        # self-attention.
+
         x = self.src_word_emb(x)
         x = self.pos_enc(x)
+        # If x_padding_mask[i,j] is True, x[i,j,:] will be masked in encoder
+        # self-attention.
         x_padding_mask = mask.get_mask_from_lengths(x_length)
         enc_output, enc_output_seq_len, _ = self.encoder(
             x, x_padding_mask, x_token_type)
         enc_output_mask = mask.get_mask_from_lengths(enc_output_seq_len)
         enc_output = self._get_speaker_embedding(enc_output, speaker)
-        (variance_adapter_output, mel_len, pitch_prediction, energy_prediction,
-         log_duration_prediction) = self.variance_adapter(
+
+        (variance_adapter_output, mel_len, pitch_prediction,
+         energy_prediction, duration_prediction) = self.variance_adapter(
              enc_output, enc_output_mask, duration_target, pitch_target,
-             energy_target)
+             energy_target, p_control, e_control, d_control)
+
         variance_adapter_output = self.pos_enc(variance_adapter_output)
         mel_mask = mask.get_mask_from_lengths(mel_len)
         dec_output, _ = self.decoder(variance_adapter_output, mel_mask)
         mel_prediction = self.linear(dec_output)
         postnet_mel_prediction = self.postnet(mel_prediction) + mel_prediction
+
         return (mel_prediction, postnet_mel_prediction, mel_mask,
-                pitch_prediction, energy_prediction, log_duration_prediction,
+                pitch_prediction, energy_prediction, duration_prediction,
                 enc_output_mask)
-
-    def inference(self,
-                  x: torch.Tensor,
-                  x_length: torch.Tensor,
-                  x_token_type: torch.IntTensor,
-                  p_control: float,
-                  e_control: float,
-                  d_control: float,
-                  speaker: torch.Tensor = None) -> torch.Tensor:
-        """Inference function of FastSpeech2.
-
-        Args:
-            x (torch.Tensor): Input phoneme sequences.
-            x_length (torch.Tensor): A Tensor of shape (b), each element is the
-            length of x.
-            x_token_type (torch.IntTensor): shape (b,t'). Assume the output of
-            the final FFT
-            block is final_FFT_output and the final output of encoder is
-            enc_output. When token_type[i,j] is 0, final_FFT_output[i,j,:] will
-            be remove from enc_output.
-            p_control (float): shape (b,t). Ground truth duration for each
-            phoneme.
-            e_control (float): shape (b,t). Ground truth pitch for each phoneme.
-            d_control (float): shape (b,t). Ground truth energy for each
-            phoneme.
-            speaker (torch.Tensor, optional): shape (b). Speaker ID for each
-            training sample. Defaults to None.
-        Returns:
-            torch.Tensor: Predicted mel.
-        """
-        # If x_padding_mask[i,j] is True, x[i,j,:] will be masked in encoder
-        # self-attention.
-        x = self.src_word_emb(x)
-        x = self.pos_enc(x)
-        x_padding_mask = mask.get_mask_from_lengths(x_length)
-        enc_output, enc_output_seq_len, _ = self.encoder.inference(
-            x, x_padding_mask, x_token_type)
-        enc_output_mask = mask.get_mask_from_lengths(enc_output_seq_len)
-        enc_output = self._get_speaker_embedding(enc_output, speaker)
-        variance_adapter_output, mel_len = self.variance_adapter.inference(
-            enc_output, enc_output_mask, p_control, e_control, d_control)
-        variance_adapter_output = self.pos_enc(variance_adapter_output)
-        mel_mask = mask.get_mask_from_lengths(mel_len)
-        dec_output, _ = self.decoder.inference(variance_adapter_output,
-                                               mel_mask)
-        mel_prediction = self.linear(dec_output)
-        mel_prediction = self.postnet(mel_prediction) + mel_prediction
-        return mel_prediction
