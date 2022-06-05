f2e4ab1709ee4afe43f873060496d8ca78ffeb27
unrea1-sama <unrea1sama@outlook.com>
2022-06-05 22:06:00 +0800

    update copyright

--- wetts/dataset/feats.py
+++ wetts/dataset/feats.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
-#
+#               2022 Tsinghua University (Jie Chen)
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
