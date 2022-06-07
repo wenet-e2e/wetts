e1471bfc4089b05c8540f4207ce076c88bbb51f5
unrea1-sama <unrea1sama@outlook.com>
2022-06-07 23:10:04 +0800

    add inference script for FastSpeech2 (#28)

---
+++ wetts/bin/inference.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2022 Tsinghua University(Jie Chen)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+
+def main():
+    if sys.argv[1] == 'fastspeech2':
+        from wetts.models.am.fastspeech2 import inference
+        inference.main(inference.get_args(sys.argv[2:]))
+    else:
+        raise ValueError("Wrong model name!")
+
+
+if __name__ == '__main__':
+    main()
