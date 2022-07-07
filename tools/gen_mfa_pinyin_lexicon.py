85b1ccff0211e2733086d00d33f93461e6a96e06
pengzhendong <275331498@qq.com>
2022-07-07 19:28:01 +0800

    [fastspeech2] Fix lint

--- tools/gen_mfa_pinyin_lexicon.py
+++ tools/gen_mfa_pinyin_lexicon.py
@@ -26,15 +26,69 @@ import re
 from collections import OrderedDict
 
 INITIALS = [
-    'b', 'p', 'm', 'f', 'd', 't', 'n', 'l', 'g', 'k', 'h', 'zh', 'ch', 'sh',
-    'r', 'z', 'c', 's', 'j', 'q', 'x'
+    'b',
+    'p',
+    'm',
+    'f',
+    'd',
+    't',
+    'n',
+    'l',
+    'g',
+    'k',
+    'h',
+    'zh',
+    'ch',
+    'sh',
+    'r',
+    'z',
+    'c',
+    's',
+    'j',
+    'q',
+    'x',
 ]
 
 FINALS = [
-    'a', 'ai', 'ao', 'an', 'ang', 'e', 'er', 'ei', 'en', 'eng', 'o', 'ou',
-    'ong', 'ii', 'iii', 'i', 'ia', 'iao', 'ian', 'iang', 'ie', 'io', 'iou',
-    'iong', 'in', 'ing', 'u', 'ua', 'uai', 'uan', 'uang', 'uei', 'uo', 'uen',
-    'ueng', 'v', 've', 'van', 'vn'
+    'a',
+    'ai',
+    'ao',
+    'an',
+    'ang',
+    'e',
+    'er',
+    'ei',
+    'en',
+    'eng',
+    'o',
+    'ou',
+    'ong',
+    'ii',
+    'iii',
+    'i',
+    'ia',
+    'iao',
+    'ian',
+    'iang',
+    'ie',
+    'io',
+    'iou',
+    'iong',
+    'in',
+    'ing',
+    'u',
+    'ua',
+    'uai',
+    'uan',
+    'uang',
+    'uei',
+    'uo',
+    'uen',
+    'ueng',
+    'v',
+    've',
+    'van',
+    'vn',
 ]
 
 SPECIALS = ['sil', 'sp']
@@ -67,9 +121,11 @@ def rule(C, V, R, T):
         return None
 
     # 齐齿呼或者撮口呼不能和 f, g, k, h, zh, ch, sh, r, z, c, s
-    if (V not in ['ii', 'iii']) and V[0] in ['i', 'v'] and (C in [
-            'f', 'g', 'k', 'h', 'zh', 'ch', 'sh', 'r', 'z', 'c', 's'
-    ]):
+    if (
+        (V not in ['ii', 'iii'])
+        and V[0] in ['i', 'v']
+        and (C in ['f', 'g', 'k', 'h', 'zh', 'ch', 'sh', 'r', 'z', 'c', 's'])
+    ):
         return None
 
     # 撮口呼只能和 j, q, x l, n 拼
@@ -84,19 +140,18 @@ def rule(C, V, R, T):
                 return None
 
     # j, q, x 只能和齐齿呼或者撮口呼拼
-    if (C in ['j', 'q', 'x'
-              ]) and not ((V not in ['ii', 'iii']) and V[0] in ['i', 'v']):
+    if (C in ['j', 'q', 'x']) and not ((V not in ['ii', 'iii']) and V[0] in ['i', 'v']):
         return None
 
     # b, p ,m, f 不能和合口呼拼，除了 u 之外
     # bm p, m, f 不能和撮口呼拼
-    if (C in ['b', 'p', 'm', 'f']) and ((V[0] in ['u', 'v'] and V != "u")
-                                        or V == 'ong'):
+    if (C in ['b', 'p', 'm', 'f']) and (
+        (V[0] in ['u', 'v'] and V != "u") or V == 'ong'
+    ):
         return None
 
     # ua, uai, uang 不能和 d, t, n, l, r, z, c, s 拼
-    if V in ['ua', 'uai', 'uang'
-             ] and C in ['d', 't', 'n', 'l', 'r', 'z', 'c', 's']:
+    if V in ['ua', 'uai', 'uang'] and C in ['d', 't', 'n', 'l', 'r', 'z', 'c', 's']:
         return None
 
     # sh 和 ong 不能拼
@@ -105,7 +160,19 @@ def rule(C, V, R, T):
 
     # o 和 gkh, zh ch sh r z c s 不能拼
     if V == "o" and C in [
-            'd', 't', 'n', 'g', 'k', 'h', 'zh', 'ch', 'sh', 'r', 'z', 'c', 's'
+        'd',
+        't',
+        'n',
+        'g',
+        'k',
+        'h',
+        'zh',
+        'ch',
+        'sh',
+        'r',
+        'z',
+        'c',
+        's',
     ]:
         return None
 
@@ -157,7 +224,7 @@ def rule(C, V, R, T):
 def generate_lexicon(with_tone=False, with_erhua=False):
     """Generate lexicon for Mandarin Chinese."""
     syllables = OrderedDict()
-    
+
     for s in SPECIALS:
         syllables[s] = s
 
@@ -185,15 +252,16 @@ def generate_symbols(lexicon):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Generate lexicon for Chinese pinyin to phoneme for MFA")
+        description="Generate lexicon for Chinese pinyin to phoneme for MFA"
+    )
     parser.add_argument("lexicon", type=str, help="Path to save lexicon.")
     parser.add_argument("phones", type=str, help="Path to save lexicon.")
-    parser.add_argument("--with-tone",
-                        action="store_true",
-                        help="whether to consider tone.")
-    parser.add_argument("--with-r",
-                        action="store_true",
-                        help="whether to consider erhua.")
+    parser.add_argument(
+        "--with-tone", action="store_true", help="whether to consider tone."
+    )
+    parser.add_argument(
+        "--with-r", action="store_true", help="whether to consider erhua."
+    )
     args = parser.parse_args()
 
     lexicon = generate_lexicon(args.with_tone, args.with_r)
