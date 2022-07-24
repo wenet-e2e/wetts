cc6d5ebe51b842d9a8f0325c9e3d9ab033ec9e23
xcmyz <xcmyz@outlook.com>
2022-07-25 04:42:46 +0800

    [BZNSYP Support] Split dataset

--- examples/BZNSYP/local/parse_data.py
+++ examples/BZNSYP/local/parse_data.py
@@ -9,6 +9,8 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', "-d", type=str, help='Path to BZNSYP dataset.')
     parser.add_argument('--save_path', "-s", type=str, help='Path to save processed data.')
+    parser.add_argument('--val_samples', "-v", type=int, default=20, help='Number of validation samples.')
+    parser.add_argument('--test_samples', "-t", type=int, default=20, help='Number of test samples.')
     return parser.parse_args()
 
 
@@ -41,31 +43,46 @@ def generate_duration(interval_info):
     return phone_list, duration_list
 
 
-def generate_data_list(data_path, save_path):
-    path = os.path.join(data_path, "PhoneLabeling")
+def generate_data_list(args):
+    path = os.path.join(args.data_path, "PhoneLabeling")
     filename_list = os.listdir(path)
     filename_list.sort()
-    with jsonlines.open(save_path, 'w') as fdatalist:
-        for filename in filename_list:
-            info_dict = parse_interval(os.path.join(path, filename))
-            key = info_dict['sentence_id']
-            relative_path = os.path.join(data_path, "Wave", f"{info_dict['sentence_id']}.wav")
-            if os.path.exists(relative_path):
-                wav = os.path.abspath(relative_path)
-                phone_list, duration_list = generate_duration(info_dict)
-                fdatalist.write({
-                    'key': key,
-                    'wav_path': wav,
-                    'speaker': 1,
-                    'text': phone_list,
-                    'duration': [float(d) for d in duration_list]
-                })
+    data_list = []
+    for filename in filename_list:
+        info_dict = parse_interval(os.path.join(path, filename))
+        key = info_dict['sentence_id']
+        relative_path = os.path.join(args.data_path, "Wave", f"{info_dict['sentence_id']}.wav")
+        if os.path.exists(relative_path):
+            wav = os.path.abspath(relative_path)
+            phone_list, duration_list = generate_duration(info_dict)
+            data_list.append({
+                'key': key,
+                'wav_path': wav,
+                'speaker': 1,
+                'text': phone_list,
+                'duration': [float(d) for d in duration_list]
+            })
+    train_data_list = data_list[args.val_samples + args.test_samples:]
+    val_data_list = data_list[:args.val_samples]
+    test_data_list = data_list[args.val_samples:args.val_samples + args.test_samples]
+    os.makedirs(args.save_path, exist_ok=True)
+    os.makedirs(os.path.join(args.save_path, "train"), exist_ok=True)
+    os.makedirs(os.path.join(args.save_path, "val"), exist_ok=True)
+    os.makedirs(os.path.join(args.save_path, "test"), exist_ok=True)
+    with jsonlines.open(os.path.join(args.save_path, "train", "datalist.jsonl"), 'w') as fdatalist:
+        for data in train_data_list:
+            fdatalist.write(data)
+    with jsonlines.open(os.path.join(args.save_path, "val", "datalist.jsonl"), 'w') as fdatalist:
+        for data in val_data_list:
+            fdatalist.write(data)
+    with jsonlines.open(os.path.join(args.save_path, "test", "datalist.jsonl"), 'w') as fdatalist:
+        for data in test_data_list:
+            fdatalist.write(data)
 
 
-def main():
-    args = get_args()
-    generate_data_list(args.data_path, args.save_path)
+def main(args):
+    generate_data_list(args)
 
 
 if __name__ == "__main__":
-    main()
+    main(get_args())
