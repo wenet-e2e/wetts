06231b883e35d975e493e412e8a1ff29c3797e1c
Zhengxi Liu (刘正曦) <xcmyz@outlook.com>
2022-08-13 10:53:20 +0800

    [examples] BZNSYP Support (#61)
    * [BZNSYP Support] Initial configuration
    
    * [BZNSYP Support] Initial add
    
    * [BZNSYP Support] Parse data and generate data list
    
    * [BZNSYP Support] Split dataset
    
    * Ignore BZNSYP
    
    * Only root path
    
    * [BZNSYP Support] Use phone duration
    
    * [BZNSYP Support] Add
    
    * [BZNSYP Support] Initial add
    
    * [BZNSYP Support] Add link
    
    * [BZNSYP Support] Update script
    
    * [BZNSYP Support] Add phone map list
    
    * [BZNSYP Support] Add speaker map list
    
    * [BZNSYP Support] Verify phone
    
    * [BZNSYP Support] Add new phone
    
    * [BZNSYP Support] Fix bug in path
    
    * [BZNSYP Support] Add special token
    
    * [BZNSYP Support] Sync with new commit
    
    * [BZNSYP Support] Fix bug
    
    * [BZNSYP Support] Fix bug
    
    * [BZNSYP Support] Format code
    
    Co-authored-by: 刘正曦 <liuzhengxi.nico@bytedance.com>

---
+++ examples/BZNSYP/run.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+# Copyright 2022 Zhengxi Liu (xcmyz@outlook.com)
+
+. path.sh
+
+stage=                            # start from -1 if you need to download data
+stop_stage=
+
+dataset_dir=                      # path to dataset directory
+
+fastspeech2_outputdir=fastspeech2
+fastspeech2_config=conf/fastspeech2.yaml
+
+hifigan_outputdir=hifigan
+hifigan_config=conf/hifigan_v1.yaml
+
+conda_base=$(conda info --base)
+source $conda_base/bin/activate wetts
+
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+  # Prepare samples
+  python local/parse_data.py \
+      --data_path $dataset_dir \
+      --save_path $fastspeech2_outputdir \
+      --val_samples 20 \
+      --test_samples 20 \
+      --phn2id_path local/phn2id
+  # Prepare lexicon.
+  python tools/gen_mfa_pinyin_lexicon.py --with-tone --with-r \
+    $fastspeech2_outputdir/lexicon.txt $fastspeech2_outputdir/phone.txt
+fi
+
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+  # Compute mel, f0, energy CMVN
+  total=$(cat $fastspeech2_outputdir/train/datalist.jsonl | wc -l)
+  python wetts/bin/compute_cmvn.py --total $total --num_workers 32 \
+      $fastspeech2_config $fastspeech2_outputdir/train/datalist.jsonl \
+      $fastspeech2_outputdir/train
+fi
+
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+  # train fastspeech2
+  EPOCH=
+  python wetts/bin/train.py fastspeech2 --num_workers 32 \
+      --config $fastspeech2_config \
+      --train_data_list $fastspeech2_outputdir/train/datalist.jsonl \
+      --val_data_list $fastspeech2_outputdir/val/datalist.jsonl \
+      --cmvn_dir $fastspeech2_outputdir/train \
+      --spk2id_file local/spk2id \
+      --phn2id_file local/phn2id \
+      --special_tokens_file local/special_token.txt \
+      --log_dir log/ \
+      --batch_size 64 \
+      --epoch $EPOCH
+fi
+
+FASTSPEECH2_INFERENCE_OUTPUTDIR=$fastspeech2_outputdir/inference_mels # path to directory for inferenced mels
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+  # inference fastspeech2
+  TEXT_FILE=test_samples.txt                 # path to text file, each line contains one sample for inference
+  SPEAKER_FILE=test_samples_speakers.txt     # path to speaker file, each line contains one speaker name for corresponding line in text file
+  FASTSPEECH2_CKPT_PATH=                     # path to fastspeech2 checkpoint file
+  python wetts/bin/inference.py fastspeech2 \
+      --num_workers 4 \
+      --batch_size 64 \
+      --config $fastspeech2_config \
+      --text_file $TEXT_FILE \
+      --speaker_file $SPEAKER_FILE \
+      --lexicon_file $fastspeech2_outputdir/lexicon.txt \
+      --cmvn_dir $fastspeech2_outputdir/train \
+      --spk2id_file local/spk2id \
+      --phn2id_file local/phn2id \
+      --special_token_file local/special_token.txt \
+      --export_dir $FASTSPEECH2_INFERENCE_OUTPUTDIR \
+      --ckpt $FASTSPEECH2_CKPT_PATH
+fi
+
+
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+  # train hifigan using fastspeech2 training dataset
+  EPOCH=                              # Number of epoch for training hifigan
+  python wetts/bin/hifigan_train.py train \
+      --num_workers 32 \
+      --batch_size_hifigan 32 \
+      --fastspeech2_train_datalist $fastspeech2_outputdir/train/datalist.jsonl \
+      --fastspeech2_val_datalist $fastspeech2_outputdir/val/datalist.jsonl \
+      --hifigan_config $hifigan_config \
+      --epoch $EPOCH \
+      --export_dir $hifigan_outputdir/train
+fi
+
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+  # finetune hifigan using fastspeech2 training dataset
+  FASTSPEECH2_CKPT_PATH=          # path to fastspeech2 checkpoint
+  HIFIGAN_CKPT_PATH=              # path to hifigan generator and discriminator checkpoint
+                                  # e.g. $HIFIGAN_CKPT_PATH='g_02500000 do_02500000'
+                                  # pretrained hifigan checkpoint can be obtained from:
+                                  # https://github.com/jik876/hifi-gan
+
+  EPOCH=                          # number of epoch for finetune
+  python wetts/bin/hifigan_train.py finetune \
+      --num_workers 32 \
+      --batch_size_hifigan 32 \
+      --batch_size_fastspeech2 32 \
+      --fastspeech2_config conf/fastspeech2.yaml \
+      --fastspeech2_train_datalist $fastspeech2_outputdir/train/datalist.jsonl \
+      --hifigan_config $hifigan_config \
+      --phn2id_file local/phn2id \
+      --spk2id_file local/spk2id \
+      --special_tokens_file local/special_token.txt \
+      --cmvn_dir $fastspeech2_outputdir/train \
+      --fastspeech2_ckpt $FASTSPEECH2_CKPT_PATH \
+      --hifigan_ckpt $HIFIGAN_CKPT_PATH \
+      --epoch $EPOCH \
+      --export_dir $hifigan_outputdir/finetune
+fi
+
+
+if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
+  # hifigan inference
+  HIFIGAN_GENERATOR_CKPT_PATH=    # path to hifigan generator checkpoint
+                                  # e.g. $HIFIGAN_GENERATOR_CKPT_PATH=g_02500000
+                                  # pretrained hifigan checkpoint can be obtained from:
+                                  # https://github.com/jik876/hifi-gan
+
+  python wetts/bin/hifigan_inference.py \
+      --num_workers 4 \
+      --batch_size 32 \
+      --config $hifigan_config \
+      --datalist $FASTSPEECH2_INFERENCE_OUTPUTDIR/fastspeech2_mel_prediction.jsonl \
+      --ckpt $HIFIGAN_GENERATOR_CKPT_PATH \
+      --export_dir $hifigan_outputdir/wavs
+fi
