0ea08089eeb6ce42167cba6c4e94a4b88477cc18
Binbin Zhang <binbzha@qq.com>
2022-10-16 15:40:28 +0800

    fix logic error

--- wetts/vits/train_ms.py
+++ wetts/vits/train_ms.py
@@ -158,9 +158,9 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler,
         speakers = speakers.cuda(rank, non_blocking=True)
 
         with autocast(enabled=hps.train.fp16_run):
-            y_hat, l_length, attn, ids_slice, x_mask, z_mask,
-            (z, z_p, m_p, logs_p, m_q, logs_q) = net_g(x, x_lengths, spec,
-                                                       spec_lengths, speakers)
+            y_hat, l_length, attn, ids_slice, x_mask, z_mask, (
+                z, z_p, m_p, logs_p, m_q,
+                logs_q) = net_g(x, x_lengths, spec, spec_lengths, speakers)
 
             mel = spec_to_mel_torch(spec, hps.data.filter_length,
                                     hps.data.n_mel_channels,
