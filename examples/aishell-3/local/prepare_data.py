55aeb31872c4d2046cec5aace1637099291a4c17
Binbin Zhang <binbzha@qq.com>
2022-11-22 22:08:17 +0800

    [examples] add aishell-3 training recipe

---
+++ examples/aishell-3/local/prepare_data.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2022 Binbin Zhang(binbzha@qq.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+
+if len(sys.argv) != 4:
+    print('Usage: prepare_data.py lexicon in_data_dir out_data')
+    sys.exit(-1)
+
+lexicon = {}
+with open(sys.argv[1], 'r', encoding='utf8') as fin:
+    for line in fin:
+        arr = line.strip().split()
+        lexicon[arr[0]] = arr[1:]
+
+train_set_label_file = os.path.join(sys.argv[2], 'train',
+                                    'label_train-set.txt')
+with open(train_set_label_file, encoding='utf8') as fin, \
+     open(sys.argv[3], 'w', encoding='utf8') as fout:
+    # skip the first five lines in label_train-set.txt
+    lines = [x.strip() for x in fin.readlines()][5:]
+    for line in lines:
+        key, text, _ = line.split('|')
+        speaker = key[:-4]
+        wav_path = os.path.join(sys.argv[2], 'train', 'wav', speaker,
+                                '{}.wav'.format(key))
+        phones = []
+        for x in text.split():
+            if x == '%' or x == '$':
+                phones.append(x)
+            elif x in lexicon:
+                phones.extend(lexicon[x])
+            else:
+                print('{} OOV {}'.format(key, x))
+                sys.exit(-1)
+        fout.write('{}|{}|{}\n'.format(wav_path, speaker, ' '.join(phones)))
---
+++ examples/aishell-3/run.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 Jie Chen
+# Copyright 2022 Binbin Zhang(binbzha@qq.com)
+
+[ -f path.sh ] && . path.sh
+
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+
+stage=0  # start from -1 if you need to download data
+stop_stage=1
+
+dataset_url=https://openslr.magicdatatech.com/resources/93/data_aishell3.tgz
+dataset_dir=. # path to dataset directory
+
+config=configs/base.json
+dir=exp/base  # training dir
+test_audio=test_audio
+
+data=data
+use_onnx=false
+
+. tools/parse_options.sh || exit 1;
+
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+  # Download data
+  local/download_data.sh $dataset_url $dataset_dir
+fi
+
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+  # Prepare data for training/validation
+  mkdir -p $data
+  python tools/gen_pinyin_lexicon.py --with-tone --with-r \
+      $data/lexicon.txt $data/phones.list
+  python local/prepare_data.py $data/lexicon.txt \
+      $dataset_dir/data_aishell3 $data/all.txt
+  cat $data/all.txt | awk -F '\|' '{print $2}' | \
+      sort | uniq | awk '{print $0, NR}' > $data/speaker.txt
+  # phone with 0 is kept for <blank>
+  cat $data/all.txt | awk -F '\|' '{print $3}' | \
+      awk '{ for (i=1;i<=NF;i++) print $i}' | \
+      sort | uniq | awk '{print $0, NR}' > $data/phones.txt
+  # Split train/validation
+  cat $data/all.txt | shuf --random-source=<(yes 777) | head -n 110 | \
+      awk -F '\|' '{print $1}' > $data/val.key
+  cat $data/all.txt | grep -f $data/val.key > $data/val.txt
+  head -10 $data/val.txt > $data/test.txt
+  sed -i '1,10d' $data/val.txt
+  cat $data/all.txt | grep -v -f $data/val.key > $data/train.txt
+fi
+
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+  export MASTER_ADDR=localhost
+  export MASTER_PORT=10087
+  python vits/train.py -c $config -m $dir \
+    --train_data $data/train.txt \
+    --val_data $data/val.txt \
+    --phone_table $data/phones.txt \
+    --speaker_table $data/speaker.txt
+fi
+
