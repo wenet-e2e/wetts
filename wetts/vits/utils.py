e480f6bfb8df4db2a63b24f5ab0c321ab59531a7
Binbin Zhang <binbzha@qq.com>
2022-11-22 21:30:51 +0800

    [vits] use INFO as default log level

--- wetts/vits/utils.py
+++ wetts/vits/utils.py
@@ -12,7 +12,7 @@ import torch
 
 MATPLOTLIB_FLAG = False
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging
 
 
@@ -250,14 +250,14 @@ def check_git_hash(model_dir):
 def get_logger(model_dir, filename="train.log"):
     global logger
     logger = logging.getLogger(os.path.basename(model_dir))
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 
     formatter = logging.Formatter(
         "%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s")
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
     h = logging.FileHandler(os.path.join(model_dir, filename))
-    h.setLevel(logging.DEBUG)
+    h.setLevel(logging.INFO)
     h.setFormatter(formatter)
     logger.addHandler(h)
     return logger
