21479f9dc5ba9dc34f46720385b855e4dee8ee98
Binbin Zhang <binbzha@qq.com>
2022-11-22 21:55:49 +0800

    [vits] refactor log dir (#100)

--- wetts/vits/utils.py
+++ wetts/vits/utils.py
@@ -185,7 +185,7 @@ def get_hparams(init=True):
                         help='speaker table, required for multiple speakers')
 
     args = parser.parse_args()
-    model_dir = os.path.join("./logs", args.model)
+    model_dir = args.model
 
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
