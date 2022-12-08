e605bf0cb95982d6163ee2a604e113f6221bcc12
彭震东 <275331498@qq.com>
2022-12-08 13:01:38 +0800

    [runtime] add frontend to tts_main (#112)
    * [runtime] add frontend to tts_main
    
    * [lint] fix flake8 lint

--- runtime/core/bin/tts_main.cc
+++ runtime/core/bin/tts_main.cc
@@ -17,39 +17,54 @@
 
 #include "gflags/gflags.h"
 #include "glog/logging.h"
+#include "processor/processor.h"
 
+#include "frontend/g2p_prosody.h"
 #include "frontend/wav.h"
 #include "model/onnx_tts_model.h"
 #include "utils/string.h"
 
-DEFINE_string(model_path, "", "model path");
-DEFINE_string(phonemes, "", "input phonemes");
-DEFINE_string(phone_dict, "", "phone dict path");
+DEFINE_string(text, "", "input text");
+
+DEFINE_string(tagger_file, "", "tagger fst file");
+DEFINE_string(verbalizer_file, "", "verbalizer fst file");
+
+DEFINE_string(g2p_prosody_model, "", "g2p prosody model file");
+DEFINE_string(phone_file, "", "phone list file");
+DEFINE_string(tokenizer_vocab_file, "", "tokenizer vocab file");
+DEFINE_string(lexicon_file, "", "lexicon file");
+
+DEFINE_string(e2e_model_file, "", "e2e tts model file");
 DEFINE_string(wav_path, "", "output wave path");
 
 int main(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, false);
   google::InitGoogleLogging(argv[0]);
 
+  wetext::Processor processor(FLAGS_tagger_file, FLAGS_verbalizer_file);
+  wetts::G2pProsody g2p_prosody(FLAGS_g2p_prosody_model, FLAGS_phone_file,
+                                FLAGS_tokenizer_vocab_file, FLAGS_lexicon_file);
   wetts::OnnxTtsModel::InitEngineThreads(1);
   auto model = std::make_shared<wetts::OnnxTtsModel>();
-  model->Read(FLAGS_model_path);
+  model->Read(FLAGS_e2e_model_file);
 
-  std::unordered_map<std::string, int64_t> phones_map;
-  std::ifstream file(FLAGS_phone_dict);
-  std::string line;
-  while (getline(file, line)) {
-    std::vector<std::string> strs;
-    wetts::SplitString(line, &strs);
-    phones_map[strs[0]] = std::stoi(strs[1]);
-  }
+  // 1. TN
+  std::string normalized_text = processor.normalize(FLAGS_text);
+  // 2. G2P: char => pinyin => phones => ids
+  std::vector<std::string> pinyins;
+  std::vector<int> prosody;
+  g2p_prosody.Compute(normalized_text, &pinyins, &prosody);
 
-  std::vector<std::string> phones;
-  wetts::SplitString(FLAGS_phonemes, &phones);
   std::vector<int64_t> inputs;
-  for (std::string phone : phones) {
-    CHECK_NE(phones_map.count(phone), 0);
-    inputs.emplace_back(phones_map[phone]);
+  for (int i = 0; i < pinyins.size(); ++i) {
+    std::vector<std::string> phonemes;
+    wetts::SplitString(pinyins[i], &phonemes);
+    for (std::string phoneme : phonemes) {
+      inputs.emplace_back(std::stoi(phoneme));
+    }
+    if (prosody[i] != 0) {
+      inputs.emplace_back(prosody[i]);
+    }
   }
 
   std::vector<float> audio;
