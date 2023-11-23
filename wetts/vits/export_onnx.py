72da4532d85993b42b601086d048a70cc1aae5a8
zhuyunfeng <42790740+zhuzizyf@users.noreply.github.com>
2023-11-23 08:12:56 -0600

    Update export_onnx.py (#180)
    * Update export_onnx.py
    
    * Update export_onnx.py
    
    fix indent

--- wetts/vits/export_onnx.py
+++ wetts/vits/export_onnx.py
@@ -51,13 +51,19 @@ def main():
     phone_num = len(open(args.phone_table).readlines())
     num_speakers = len(open(args.speaker_table).readlines())
 
+    posterior_channels = hps.data.filter_length // 2 + 1
+    if ("use_mel_posterior_encoder" in hps.model.keys()
+            and hps.model.use_mel_posterior_encoder):
+        print("Using mel posterior encoder for VITS2")
+        posterior_channels = 80  # vits2
     net_g = SynthesizerTrn(phone_num,
-                           hps.data.filter_length // 2 + 1,
+                           posterior_channels,
                            hps.train.segment_size // hps.data.hop_length,
                            n_speakers=num_speakers,
                            **hps.model)
     task.load_checkpoint(args.checkpoint, net_g, None)
-    net_g.flow.remove_weight_norm()
+    if hasattr(net_g.flow, 'remove_weight_norm'):
+        net_g.flow.remove_weight_norm()
     net_g.dec.remove_weight_norm()
     net_g.forward = net_g.export_forward
     net_g.eval()
