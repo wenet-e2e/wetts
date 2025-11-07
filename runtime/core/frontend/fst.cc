0af9a1cb4e80614664a8b0945a1559d17c4e9479
Binbin Zhang <binbzha@qq.com>
2025-11-07 16:22:14 +0800

    [runtime] mv fst to frontend

--- runtime/core/frontend/fst.cc
+++ runtime/core/frontend/fst.cc
@@ -12,7 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "utils/fst.h"
+#include "frontend/fst.h"
+
+#include <string>
+#include <vector>
 
 #include "fst/rmepsilon.h"
 
